7a3ee91f1eba3d168842fada62e4b137ee3a0f1c
JungJinUk01 <128438814+JinukSynth@users.noreply.github.com>
2024-05-28 10:25:32 +0900

    Update project files

--- trp_vehicle_manager/settings.py
+++ trp_vehicle_manager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-c+c7oamz+#v--75g%wid&+kk6j2jk)e%(*$5w!pje6)%nz^(zt"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
